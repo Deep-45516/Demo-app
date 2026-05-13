46828bbbb1e6123ef35732c761790f8a736f07bb
Deep <deeplokhande77@gmail.com>
2026-05-13 18:08:44 +0530

    backend

--- Backend/src/utils/uploadTOFirebase.js
+++ Backend/src/utils/uploadTOFirebase.js
@@ -1,17 +1,25 @@
-import { bucket } from "../config/firebase.js";
 import confessionRouter from "../routes/confession.route.js";
+import { bucket } from "../config/firebase.js";
+import crypto from "crypto";
 
 export const uploadImage = async (filePath, to) => {
-  const date = new Date().toISOString().split("T")[0];
 
-  const cleanTo = (to || "someone")
-  typeof value === "string"
-  .replace(/\s+/g, "-")
-  .toLowerCase();
+  const date =
+    new Date().toISOString().split("T")[0];
 
-  const unique = crypto.randomUUID().slice(0, 8);
+  const cleanTo =
+    typeof to === "string" && to.trim()
+      ? to
+          .trim()
+          .replace(/\s+/g, "-")
+          .toLowerCase()
+      : "someone";
 
-  const fileName = `confessions/${cleanTo}-${date}-${unique}.png`;
+  const unique =
+    crypto.randomUUID().slice(0, 8);
+
+  const fileName =
+    `confessions/${cleanTo}-${date}-${unique}.png`;
 
   await bucket.upload(filePath, {
     destination: fileName,
