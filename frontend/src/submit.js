9dee420093fb6e1de78d292bb2855b21b22f254f
Deep <deeplokhande77@gmail.com>
2026-05-14 13:52:27 +0530

    amin auth-2

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,29 +1,48 @@
 const API = import.meta.env.VITE_BACKEND_URL;
 
-export async function submitConfession(to, from, message,userEmail) {
+export async function submitConfession(
+  to,
+  from,
+  message,
+  userEmail
+) {
+
   console.log({
     to,
     from,
     message,
-    userEmail,
-
+    userEmail
   });
 
   try {
-    const res = await fetch(`${API}/api/v1/confessions`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ to, from, message, userEmail }),
-    });
+
+    const res = await fetch(
+      `${API}/api/v1/confessions`,
+      {
+        method: "POST",
+
+        headers: {
+          "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+          to,
+          from,
+          message,
+          userEmail
+        })
+      }
+    );
 
     const data = await res.json();
 
     console.log(data);
 
     alert("Submitted!");
+
   } catch (error) {
+
     console.error(error);
+
   }
 }
