2be7cacf281c816889ad87afa5c5b490dc45fcc0
Deep <deeplokhande77@gmail.com>
2026-06-07 22:45:54 +0530

    use firebase service account json env

--- Backend/src/config/firebase.js
+++ Backend/src/config/firebase.js
@@ -4,6 +4,9 @@ const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
+console.log("Firebase project:", serviceAccount.project_id);
+console.log("Firebase email:", serviceAccount.client_email);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   storageBucket: "confessionvault.firebasestorage.app"
