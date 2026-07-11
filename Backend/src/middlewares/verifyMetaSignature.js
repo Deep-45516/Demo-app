fabf2e314611ad5dc7bf811fe9bebc7293784288
Deep <deeplokhande77@gmail.com>
2026-07-11 21:33:22 +0530

    fixing signature bug

--- Backend/src/middlewares/verifyMetaSignature.js
+++ Backend/src/middlewares/verifyMetaSignature.js
@@ -27,6 +27,7 @@ console.log("Raw Body Exists:", !!req.rawBody);
 
   if (signature !== expected) {
     console.log("❌ Signature mismatch");
+    console.log(req.rawBody.toString("utf8"));
     return res.sendStatus(401);
   }
   console.log("✅ Signature verified");
