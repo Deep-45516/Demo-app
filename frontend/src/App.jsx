3bc576ef59a5540d5eb8576f3293ff1bd32abf3c
Deep <deeplokhande77@gmail.com>
2026-07-23 23:20:59 +0530

    fixing inbox visible1234567

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,28 +4,6 @@ import Home from "./pages/Home.jsx";
 import Admin from "./pages/Admin.jsx";
 import InstagramVerification from "./components/InstagramVerification/InstagramVerification.jsx";
 import Inbox from "./pages/Inbox.jsx";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-
-      <Route path="/admin" element={<Admin />} />
-      <Route path="/instagram" element={<InstagramVerification />} />
-      <Route
-    path="/inbox"
-    element={<Inbox />}
-/>
-    </Routes>
-  );
-}
-
-export default App;import { Routes, Route } from "react-router-dom";
-
-import Home from "./pages/Home.jsx";
-import Admin from "./pages/Admin.jsx";
-import InstagramVerification from "./components/InstagramVerification/InstagramVerification.jsx";
-import Inbox from "./pages/Inbox.jsx";
 import ConfessionDetails from "./pages/ConfessionDetails.jsx";
 
 function App() {
