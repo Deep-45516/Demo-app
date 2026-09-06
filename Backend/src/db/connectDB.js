a1774f774acd030a292a9bb3d382c973515e8a0b
Deep <deeplokhande77@gmail.com>
2026-09-06 17:29:23 +0530

    no jwt,not mongo, no logic 1

--- Backend/src/db/connectDB.js
+++ Backend/src/db/connectDB.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
@@ -11,3 +12,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
+export default connectDB;
